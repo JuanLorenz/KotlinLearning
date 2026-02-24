59653212e3bd1c23812dc7fd3a1cb3c6115d1917
JuanLorenz <juanlorenzo.rebusit@gmail.com>
2026-02-24 23:20:24 +0800

    First commit for this project, help my soul

---
+++ app/src/main/java/com/example/rebusit_test/LoginPageActivity.kt
@@ -0,0 +1,10 @@
+package com.example.rebusit_test
+import android.os.Bundle
+import android.app.Activity
+
+class LoginPageActivity : Activity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.loginpage);
+    }
+}
